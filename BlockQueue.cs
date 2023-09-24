4f051d50af3a3a72ae49d28a6f0ea6f936b3248c
Gonzalo Aguado <gonziscagudo@gmail.com>
2023-09-24 19:15:57 +0200

    Initial commit

---
+++ BlockQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+
+        };
+        private readonly Random rand = new Random();
+
+        public Block NextBloack { get; private set; }
+
+        public BlockQueue() { 
+        
+            NextBloack =RandomBlock();
+       }
+
+        private Block RandomBlock()
+        {
+            return blocks[rand.Next(blocks.Length)];
+        }
+        public Block GetAndUpdate()
+        {
+            Block block = NextBloack;
+            do
+            {
+                NextBloack= RandomBlock();
+            }
+            while (block.Id== NextBloack.Id);
+            return block;
+        }
+
+    }
+}
